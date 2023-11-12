5d145d4d97eadccb0d60d7909764c30d3cad6f13
AsianMustache <phany280687@mail.ru>
2023-11-12 19:44:14 +0300

    feat: add new route /sign-in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from '../images/logo.svg';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return(
     <header className="header">
             <a href="/" className="header__logo"><img src={logo} alt="Место - Россия" className="header__logo-image" /></a>
+            <Link to="/sign-in" className="header__signin">Войти</Link>
         </header>
         )
 }

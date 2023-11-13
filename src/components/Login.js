f009bfcbd7ad0395f6d6ecc03daa1db4a8ff8bb4
AsianMustache <phany280687@mail.ru>
2023-11-13 21:26:19 +0300

    feat: add Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Login() {
     return(
         <div className="login">
-            <h2 className="login__title">Регистрация</h2>
+            <h2 className="login__title">Вход</h2>
             <label className="login__label">
             <input type="text" id="email" name="email" placeholder="Email" className="login__input-email" minLength="2" maxLength="40" required />
             <div className="login__line"></div>
@@ -13,9 +13,7 @@ function Login() {
                 <input type="password" id="password"  name="password" placeholder="Пароль" className="login__input-password" minLength="2" maxLength="200" required />
                 <div className="login__line"></div>
             </label>
-            <button className="login__button-signup"><Link to="/sign-up" className="login__button-text">Зарегистрироваться</Link></button>
-            <p>Уже зарегистрированы?</p>
-            <Link to="/sign-in">Войти</Link>
+            <button className="login__button-signin"><Link to="/sign-in" className="login__button-text">Войти</Link></button>
         </div>
     )
 }

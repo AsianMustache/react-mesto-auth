47126bc2745e3acf4e6905f4b5fc00210be363fe
AsianMustache <phany280687@mail.ru>
2023-11-19 16:15:57 +0300

    fix: fix Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,32 +5,11 @@ import { Link, useLocation } from 'react-router-dom'
 function Header({loggedIn, userEmail, onSignOut}) {
     const location = useLocation();
 
-    // const headerOption = () => {
-    //     if (location.pathname === '/sign-in') {
-    //         return <Link to="/sign-up" className="header__signin">Регистрация</Link>;
-    //     } else if (location.pathname === '/sign-up') {
-    //         return <Link to="/sign-in" className="header__signin">Войти</Link>;
-    //     } 
-    //     else if (!loggedIn) {
-    //         return (
-    //             <>
-    //                 <Link to="#" className="header__signin" value={email}>{email}</Link>
-    //                 <Link to="/sign-in" className="header__signin" onClick={onSignOut}>Выйти</Link>
-    //             </>
-    //         )
-    //     }
-    // }
-
     return(
         <header className="header">
             <Link to="/" className="header__logo">
                 <img src={logo} alt="Место - Россия" className="header__logo-image" />
             </Link>
-            {/* <p className="header__signin">{email}</p>
-            {loggedIn && <Link to="/sign-in" className="header__signin" onClick={onSignOut}>Выйти</Link>}
-            
-            {location.pathname.includes('sign-in') && <Link to="/sign-up" className="header__signin">Регистрация</Link>}
-            {location.pathname.includes('sign-up') && <Link to="/sign-in" className="header__signin">Войти</Link>} */}
             {loggedIn ? (
                 <>
                     <p className="header__signin">{userEmail}</p>
